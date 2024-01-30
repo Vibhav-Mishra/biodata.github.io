c267057f6bc3d0f78516ae355f2e617a9738bf94
Vibhav Kumar Mishra <apkavibhu@gmail.com>
2024-01-30 18:22:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function openFullscreen() {
+    document.getElementById('fullscreen').style.display = 'flex';
+  }
+
+  function closeFullscreen() {
+    document.getElementById('fullscreen').style.display = 'none';
+  }

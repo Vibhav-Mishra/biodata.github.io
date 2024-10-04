69dcb6716ddf0550e18f6e75e87551df42ed4c45
Vibhav Kumar Mishra <apkavibhu@gmail.com>
2024-10-04 12:22:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,18 @@ function closeFullscreen() {
   var fullscreenContainer = document.getElementById('fullscreenContainer');
   fullscreenContainer.style.display = 'none';
 }
+
+function toggleLanguage() {
+  const languageToggle = document.getElementById('languageToggle');
+  const language = languageToggle.innerText;
+  
+  if (language === 'Hindi') {
+    document.getElementById('dobLabel').innerText = 'जन्म तिथि:';
+    document.getElementById('dobValue').innerText = '30/05/1997 - 07:56 PM (रात)';
+    languageToggle.innerText = 'English';
+  } else {
+    document.getElementById('dobLabel').innerText = 'DOB:';
+    document.getElementById('dobValue').innerText = '30/05/1997 - 07:56 PM (night)';
+    languageToggle.innerText = 'Hindi';
+  }
+}
